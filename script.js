e89346dfd912270f53b7f5659e20aeb04a530a70
Jaibock Lee <jaibocklee@gmail.com>
2022-01-08 18:28:33 -0600

    initialized starter code and outline

---
+++ script.js
@@ -0,0 +1,25 @@
+function createScreen(dimension) {
+    // TODO: if there are children elements within container, delete them all
+
+    // TODO: calculate div size within 700px container
+
+    // TODO: square dimension for total divs
+
+        // TODO: make the styling in CSS Grid form, and 
+        //       give it white color class, 
+        //       give it a 'pixelDiv' class
+}
+
+// TODO: querySelectAll the pixelDivs class divs, and for each pixelDiv, add an changeColor event
+
+    // TODO: add eventlistener for changing color class when hovered over 
+    // TODO: lookup JavaScript hover event conditions
+
+
+// TODO: query document select the Clear button, and add an eventlistener clearScreen  
+function clearScreen() {
+    // TODO: querySelectAll pixelDivs, and assign them the white color class
+}
+
+// TODO: query document select the Dimensions input field, and add an eventlistener
+// TODO: make the event listener call createScreen with specific dimension
